5c9fceced9cd1ee1ca3cca636f2ba83bc3ad2d06
Skewnart <corentinz@outlook.fr>
2024-11-28 23:52:41 +0100

    feat: First upload / constant file

---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define DEF_SSID          "SSID"
+#define DEF_WIFIPWD       "PWD"
+#define DEF_REMOTEIP      "PUBLIC-IP"
+#define DEF_REMOTEWSPORT    "REMOTE-WEBSOCKET-PORT"
+
+#endif
